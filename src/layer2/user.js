56984c8305a2b486a4b09799d2ab754620f4a45d
Jacky.li <liyangwood@gamil.com>
2022-11-13 02:55:08 +0800

    bump

--- src/layer2/user.js
+++ src/layer2/user.js
@@ -53,7 +53,7 @@ const F = {
     const address = self.layer1_account.address;
 
     const chain = await self.wf.layer1.getChain();
-    if(true || chain.name !== 'Goerli'){
+    if(chain.name !== 'Goerli'){
       throw('Current epoch only accept Goerli network. <br> please visit <a href="https://www.youtube.com/watch?v=nsAuqfAQCag" target="_blank">this link<a> to config.');
     }
 
